a6b761fab86418955c5a0700bcbff7349e5b0a44
Ariana A <aadili243@gmail.com>
2020-11-10 18:33:32 -0800

    Revised html, css, php and js and added maps.js

--- adili.ariana/js/pages.js
+++ adili.ariana/js/pages.js
@@ -1,5 +1,31 @@
 
-const RecentPage = async() => {}
+const RecentPage = async() => {
+
+   let d = await query({
+      type:'recent_locations',
+      params:[sessionStorage.userId]
+   });
+
+   console.log(d)
+
+   let valid_animals = d.result.reduce((r,o)=>{
+      o.icon = o.img;
+      if(o.lat && o.lng) r.push(o);
+      return r;
+   },[])
+
+
+   let map_el = await makeMap("#recent-page .map");
+
+   //console.log(map_el.data('map'))
+
+   makeMarkers(map_el,valid_animals);
+
+}
+
+
+
+
 
 // async and await
 const ListPage = async() => {
@@ -10,8 +36,14 @@ const ListPage = async() => {
 
    console.log(d)
 
-   $("#list-page .animallist").html(makeAnimalList(d.result));
+   $("#list-page .animallist")
+      .html(makeAnimalList(d.result));
 }
+
+
+
+
+
 
 const UserProfilePage = async() => {
    let d = await query({
@@ -21,16 +53,33 @@ const UserProfilePage = async() => {
 
    console.log(d)
 
-   $("#user-profile-page .profile").html(makeUserProfile(d.result));
+   $("#user-profile-page .profile")
+      .html(makeUserProfile(d.result));
 }
 
+
+
+
+
+
 const AnimalProfilePage = async() => {
-   let d = await query({
+   query({
       type:'animal_by_id',
       params:[sessionStorage.animalId]
+   }).then(d=>{
+      console.log(d)
+
+      $("#animal-profile-page .profile")
+         .html(makeAnimalProfile(d.result));
    });
 
-   console.log(d)
-
-   $("#animal-profile-page .profile").html(makeAnimalProfile(d.result));
+   query({
+      type:'locations_by_animal_id',
+      params:[sessionStorage.animalId]
+   }).then(d=>{
+      makeMap("#animal-profile-page .map").then(map_el=>{
+         makeMarkers(map_el,d.result);
+      })
+   })
+   
 }
